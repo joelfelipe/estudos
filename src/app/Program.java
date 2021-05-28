242b09d8529c585c46ebbdbe5b6facab194aa136
joelfelipe <joelfelipe@gmail.com>
2021-05-28 16:29:01 -0300

    projeto de estudo

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,9 +1,60 @@
 package app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.Departamento;
+import entities.HoraContrato;
+import entities.Trabalhador;
+import entities.enums.NivelTrabalhador;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.print("Nome do Departamento: ");
+		String nomeDepartamento = sc.nextLine();
+		System.out.println("Dados do Funcionário:");
+		System.out.print("Nome: ");
+		String nomeTrabalhador = sc.nextLine();
+		System.out.print("Nível: ");
+		String nivelTrabalhador = sc.nextLine();
+		System.out.print("Salário Base: ");
+		double salarioBase = sc.nextDouble();
+		
+		Trabalhador trabalhador = new Trabalhador(nomeTrabalhador, NivelTrabalhador.valueOf(nivelTrabalhador), salarioBase, new Departamento(nomeDepartamento));
+		
+		System.out.print("Quantos contratos para este trablhador? ");
+		int n = sc.nextInt();
+
+		for (int i=1; i<=n; i++) {
+			System.out.println("Entre com o contrato #" + i + ":");
+			System.out.print("Data (DD/MM/YYYY): ");
+			Date dataContrato = sdf.parse(sc.next());
+			System.out.print("Valor por hora: ");
+			double valorPorHora = sc.nextDouble();
+			System.out.print("Duração (horas): ");
+			int horas = sc.nextInt();
+			HoraContrato contrato = new HoraContrato(dataContrato, valorPorHora, horas);
+			trabalhador.adicionarContrato(contrato);
+		}
+
+		System.out.println();
+		System.out.print("Entre com o mês e ano para calcular a renda (MM/YYYY): ");
+		String mesEAno = sc.next();
+		int mes = Integer.parseInt(mesEAno.substring(0, 2));
+		int ano = Integer.parseInt(mesEAno.substring(3));
+		System.out.println("Nome: " + trabalhador.getNome());
+		System.out.println("Departamento: " + trabalhador.getDepartamento().getNome());
+		System.out.println("Renda no mês " + mesEAno + ": " + String.format("%.2f", trabalhador.renda(ano, mes)));
+		
+		
+		sc.close();
 
 	}
 
--- src/entities/Trabalhador.java
+++ src/entities/Trabalhador.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import entities.enums.NivelTrabalhador;
@@ -10,6 +12,73 @@ public class Trabalhador {
 	private Double salarioBase;
 	
 	private Departamento departamento;
-	private List<HoraContrato> contratos;
+	private List<HoraContrato> contratos = new ArrayList<>();
 	
+	public Trabalhador() {
+	}
+	
+	public Trabalhador(String nome, NivelTrabalhador nivel, Double salarioBase, Departamento departamento) {
+		this.nome = nome;
+		this.nivel = nivel;
+		this.salarioBase = salarioBase;
+		this.departamento = departamento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public NivelTrabalhador getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(NivelTrabalhador nivel) {
+		this.nivel = nivel;
+	}
+
+	public Double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public List<HoraContrato> getContratos() {
+		return contratos;
+	}
+	
+	public void adicionarContrato(HoraContrato contrato) {
+		contratos.add(contrato);
+	}
+	
+	public void removerContrato(HoraContrato contrato) {
+		contratos.remove(contrato);
+	}
+	
+	public double renda(int ano, int mes) {
+		double soma = salarioBase;
+//		c.getDate().getMonth();
+//		c.getDate().getYear();
+		Calendar cal = Calendar.getInstance();
+		for (HoraContrato c: contratos) {
+			cal.setTime(c.getDate());
+			if((cal.get(Calendar.MONTH) + 1) == mes && cal.get(Calendar.YEAR) == ano) {
+				soma += c.valorTotal();
+			}
+		}
+		return soma;
+	}
 }
